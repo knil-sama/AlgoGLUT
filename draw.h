3d1876e81c7430b49e5443b26867fe58a7a9f98a
knil-sama <cols.knil@gmail.com>
2012-10-12 00:13:55 +0300

    Update draw.h
    add two prototypes

--- draw.h
+++ draw.h
@@ -8,5 +8,7 @@ void bresenhamG(int xa,int ya,int xb,int yb,float *c);
 void bresenham(int xa,int ya,int xb,int yb,float *c);
 void traceCercle(int xo, int yo, int r, float *c);
 float * discretisation(int m);
+point * calculPointsCourbe(int m,int n, float *T, point *P);
+point calculDunPointCourbe(float t, int n, point *P);
 
 #endif
